a5a131933caa961ed393eb7fc177b24765214271
Jacob Sayles <jsayles@gmail.com>
2020-05-21 14:37:47 -0700

    adding apache config for localhost for archiver

--- webapp/settings.py
+++ webapp/settings.py
@@ -45,7 +45,7 @@ SITE_ID = 1
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.8/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    "localhost",  # Archiving API from Mailman, keep it.
+    "127.0.0.1",  # Archiving API from Mailman, keep it.
     "lists.openlunar.org",
     "lists.moondialogs.org",
 ]
